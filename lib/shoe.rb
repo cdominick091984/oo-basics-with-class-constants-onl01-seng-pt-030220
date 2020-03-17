80b9b8cec498b6d03cc96da618846138860bdd3a
Domenico Cascone <dcascone84@gmail.com>
2020-03-17 21:00:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand,
+  attr_reader :brand
 
   BRANDS = []
 
@@ -13,7 +13,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-
-
 end
